dc933798065b4fe38c7ed9102f75c20f2417039a
nickbaz <nicobaz010@live.ca>
2018-03-07 21:15:06 -0500

    Add the main project
    - Add the other project to test the API.
    - Add the 2 classes that refer to the class of the API

---
+++ ProgrammeFideliteMainApplication/src/programmefidelitemainapplication/CustomerSuccursaleMembre.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programmefidelitemainapplication;
+
+/**
+ * Jersey REST client generated for REST resource:SuccursalemembreFacadeREST
+ * [com.programmeFidelite.succursalemembre]<br>
+ * USAGE:
+ * <pre>
+ *        CustomerSuccursaleMembre client = new CustomerSuccursaleMembre();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author Nicolas
+ */
+public class CustomerSuccursaleMembre {
+    
+}
--- ProjetFoodoraCrosemontAPI/src/java/com/modele/classes/service/SuccursalemembreFacadeREST.java
+++ ProjetFoodoraCrosemontAPI/src/java/com/modele/classes/service/SuccursalemembreFacadeREST.java
@@ -38,14 +38,14 @@ public class SuccursalemembreFacadeREST extends AbstractFacade<Succursalemembre>
 
     @POST
     @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
     public void create(Succursalemembre entity) {
         super.create(entity);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
     public void edit(@PathParam("id") Integer id, Succursalemembre entity) {
         super.edit(entity);
     }
@@ -58,21 +58,21 @@ public class SuccursalemembreFacadeREST extends AbstractFacade<Succursalemembre>
 
     @GET
     @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public Succursalemembre find(@PathParam("id") Integer id) {
         return super.find(id);
     }
 
     @GET
     @Override
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public List<Succursalemembre> findAll() {
         return super.findAll();
     }
 
     @GET
     @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
     public List<Succursalemembre> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
     }
