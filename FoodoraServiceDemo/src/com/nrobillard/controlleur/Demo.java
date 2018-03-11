c2a259ca92631bf9cc66326d800b7c2cf59d5f5b
NicolasRob <robillardnic@gmail.com>
2018-03-11 13:13:02 -0400

    Début de la migration vers une utilisation d'objet plutot que de json

--- FoodoraServiceDemo/src/com/nrobillard/controlleur/Demo.java
+++ FoodoraServiceDemo/src/com/nrobillard/controlleur/Demo.java
@@ -1,7 +1,10 @@
 
 package com.nrobillard.controlleur;
 
+import com.nrobillard.modele.FoodoraSuiviWebRequests;
 import static com.nrobillard.modele.FoodoraSuiviWebRequests.GetSuiviCommandeByNum;
+import static com.nrobillard.modele.FoodoraSuiviWebRequests.ModifySuiviCommande;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Demo
@@ -12,6 +15,24 @@ public class Demo
         System.out.println("JSON de la commmande 12345678 : ");
         JSONObject ResultatJSON = GetSuiviCommandeByNum().getBody().getObject();
         System.out.println(ResultatJSON);
+        System.out.println(ResultatJSON.toString());
+        
+        System.out.println("Liste des suivis de commandes : ");
+        JSONArray ListeSuivi = FoodoraSuiviWebRequests.GetAllSuiviCommande().getBody().getArray();
+        System.out.println(ListeSuivi);
+        System.out.println(ListeSuivi.toString());
+        
+        System.out.println("Modification du status de la commande 12345678 de Complet à En Cours: ");
+        JSONObject NewSuivi = 
+            new JSONObject("{\"emplacement\":\"40.714224,-73.961452\",\"numero\":\"doop\",\"dateCommande\":\"2018-02-19T08:18:37\",\"dateComplet\":\"dddd-02-20T02:19:29\",\"status\":\"Complet\"}");
+        int ResultatModif = ModifySuiviCommande(NewSuivi).getStatus();
+        System.out.println("Résultat de la modification: ");
+        System.out.println(ResultatModif);
+        
+        System.out.println("JSON de la commmande 12345678 : ");
+        ResultatJSON = GetSuiviCommandeByNum().getBody().getObject();
+        System.out.println(ResultatJSON);
+        System.out.println(ResultatJSON.toString());
     }
     
-}
+} 
--- FoodoraServiceDemo/src/com/nrobillard/modele/FoodoraSuiviWebRequests.java
+++ FoodoraServiceDemo/src/com/nrobillard/modele/FoodoraSuiviWebRequests.java
@@ -4,8 +4,12 @@
  */
 package com.nrobillard.modele;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mashape.unirest.http.*;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import java.io.IOException;
+import org.json.JSONObject;
+import 
 
 /**
  *
@@ -13,7 +17,43 @@ import com.mashape.unirest.http.exceptions.UnirestException;
  */
 public class FoodoraSuiviWebRequests
 {
-    public static HttpResponse<JsonNode> GetSuiviCommandeByNum() {
+    private static final String url_suivi = 
+        "http://localhost:8080/FoodoraServiceSuivi/webresources/suivi";
+               
+    public FoodoraSuiviWebRequests() 
+    {
+        Unirest.setObjectMapper(new ObjectMapper() 
+        {
+          private com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper =
+              new com.fasterxml.jackson.databind.ObjectMapper();
+
+          public <T> T readValue(String value, Class<T> valueType) 
+          {
+            try 
+            {
+              return jacksonObjectMapper.readValue(value, valueType);
+            } 
+            catch (IOException e) 
+            {
+              throw new RuntimeException(e);
+            }
+          }
+
+          public String writeValue(Object value) 
+          {
+            try 
+            {
+              return jacksonObjectMapper.writeValueAsString(value);
+            } 
+            catch (JsonProcessingException e) 
+            {
+              throw new RuntimeException(e);
+            }
+          }
+        });
+    }
+    
+    public HttpResponse<SuiviCommande> GetSuiviCommandeByNum() {
         HttpResponse<JsonNode> response = null;
         try 
         {
@@ -29,4 +69,43 @@ public class FoodoraSuiviWebRequests
         }
         return response;
     }
+    
+    public HttpResponse<JsonNode> GetAllSuiviCommande() {
+        HttpResponse<JsonNode> response = null;
+        try 
+        {
+            response =
+                Unirest.get("http://localhost:8080/FoodoraServiceSuivi/webresources/suivi")
+                .header("accept", "application/json")
+                .asJson();
+        } 
+        catch (UnirestException uex) 
+        {
+            System.out.println("La requête a déclenché une exception.");
+            System.out.println("Message: " + uex.getMessage());
+        }
+        return response;
+    }
+    
+    public HttpResponse<JsonNode> ModifySuiviCommande(JSONObject NewSuivi) {
+        HttpResponse<JsonNode> response = null;
+        try 
+        {
+            response =  
+                Unirest.put("http://localhost:8080/FoodoraServiceSuivi/webresources/suivi/12345678")
+                .header("accept", "application/json")
+                .header("Content-Type", "application/json")
+                .body(NewSuivi)
+                .asJson();
+        } 
+        catch (UnirestException uex) 
+        {
+            System.out.println("La requête a déclenché une exception.");
+            System.out.println("Message: " + uex.getMessage());
+            System.out.println("La modification a été annulé");
+        }
+        return response;
+    }
+    
+    
 }
